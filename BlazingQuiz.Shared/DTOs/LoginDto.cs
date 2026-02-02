5d5a97c8c093a84d9e7663b103daa0970af84f67
sandy <sandy.theodorus@gmail.com>
2026-02-02 13:52:08 +0700

    add login page

--- BlazingQuiz.Shared/DTOs/LoginDto.cs
+++ BlazingQuiz.Shared/DTOs/LoginDto.cs
@@ -1,11 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazingQuiz.Shared.DTOs;
 
 public class LoginDto
 {
-
+    [Required, EmailAddress, DataType(DataType.EmailAddress)]
     public string Username { get; set; }
+
+    [Required]
     public string Password { get; set; }
 
 }
