6ad1ac78b0b5300b758bab659d2ce72e22ab610b
sandy <sandy.theodorus@gmail.com>
2026-02-03 19:28:25 +0700

    implementing login ui in Web project

--- BlazingQuiz.Api/Program.cs
+++ BlazingQuiz.Api/Program.cs
@@ -2,8 +2,10 @@ using BlazingQuiz.Api.Data;
 using BlazingQuiz.Api.Data.Entities;
 using BlazingQuiz.Api.Endpoints;
 using BlazingQuiz.Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +23,38 @@ builder.Services.AddDbContext<QuizContext>(options =>
 
 builder.Services.AddTransient<IPasswordHasher<User>, PasswordHasher<User>>();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        var secretKey = builder.Configuration.GetValue<string>("Jwt:Secret");
+        var symmetricKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey));
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = symmetricKey,
+            ValidIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Jwt:Audience"),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(p =>
+    {
+        var allowedOriginStr = builder.Configuration.GetValue<string>("AllowedOrigin");
+        var allowedOrigin = allowedOriginStr.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        p.WithOrigins(allowedOrigin).AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 builder.Services.AddTransient<AuthService>();
 
 var app = builder.Build();
@@ -37,11 +71,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "My API v1");
-        options.RoutePrefix = "";
+        options.RoutePrefix = string.Empty;
     });
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
 
 app.MapAuthEndpoints();
 
